f2845371e40d1c79eb11872af42876449eb69b9b
pjskrishna <84233495+pjskrishna@users.noreply.github.com>
2023-08-04 12:46:49 +0530

    initial test commit

--- src/main/java/com/incubyte/SpacecraftTest.java
+++
@@ -1,5 +0,0 @@
-package com.incubyte;
-
-public class SpacecraftTest {
-
-}
---
+++ src/test/java/com/incubyte/SpacecraftTest.java
@@ -0,0 +1,14 @@
+package com.incubyte;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class SpacecraftTest {
+	@Test
+	//methods should be public
+	public void demoTestMethod()
+	{
+		assertTrue(true);
+	}
+}
