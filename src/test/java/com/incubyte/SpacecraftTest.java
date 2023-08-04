e1a6bef2683f299b582829a3d97195a1dad30c07
pjskrishna <84233495+pjskrishna@users.noreply.github.com>
2023-08-04 13:13:49 +0530

    failed test checking

--- src/test/java/com/incubyte/SpacecraftTest.java
+++ src/test/java/com/incubyte/SpacecraftTest.java
@@ -1,5 +1,6 @@
 package com.incubyte;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -7,8 +8,20 @@ import org.junit.Test;
 public class SpacecraftTest {
 	@Test
 	//methods should be public
+	//sample test case for initial testing
 	public void demoTestMethod()
 	{
 		assertTrue(true);
+	}
+	
+	
+	//Initially failed: Expected (0,1,0) Actual (0,0,0)
+	@Test
+	public void testMoveForward()
+	{
+		Spacecraft spacecraft = new Spacecraft(0,0,0,Direction.N);
+		
+		spacecraft.moveForward();
+		assertEquals("(0,1,0)",spacecraft.currentPosition());
 	}
 }
